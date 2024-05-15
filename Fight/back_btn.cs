73a6e9923c2b62db1ac494e7deb75e6761b6996c
[NoNamed02] <[knite7989@gmail.com]>
2024-05-15 20:41:09 +0900

    change

--- Fight/back_btn.cs
+++ Fight/back_btn.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class back_btn : MonoBehaviour
+public class back_btn : MonoBehaviour // test
 {
     // Start is called before the first frame update
     public void btn()
