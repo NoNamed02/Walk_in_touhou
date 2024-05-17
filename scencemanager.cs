39946098340d697312eaee01a05257b726a43d5d
[NoNamed02] <[knite7989@gmail.com]>
2024-05-18 01:11:29 +0900

    필요없는 부분 제거

--- scencemanager.cs
+++ scencemanager.cs
@@ -14,7 +14,6 @@ public class scencemanager : MonoBehaviour
         int deviceW = Screen.width;
         int deviceH = Screen.height;
 
-        //if((float)(setWidth/setHeight) > (float)(deviceW / deviceH)){}
         Screen.SetResolution(setWidth, (int)((float)deviceH / deviceW * setWidth), true);
 
         if((float) setWidth / setHeight < (float) deviceW / deviceH){
@@ -28,25 +27,6 @@ public class scencemanager : MonoBehaviour
     }
     void Start()
     {
-        /*
-        int setWidth = 1440;
-        int setHeight = 3088;
-        Screen.orientation = ScreenOrientation.Portrait;
-        int deviceW = Screen.width;
-        int deviceH = Screen.height;
-
-        //if((float)(setWidth/setHeight) > (float)(deviceW / deviceH)){}
-        Screen.SetResolution(setWidth, (int)((float)deviceH / deviceW * setWidth), true);
-
-        if((float) setWidth / setHeight < (float) deviceW / deviceH){
-            float newW = ((float)setWidth / setHeight) / ((float)deviceW / deviceH);
-            Camera.main.rect = new Rect((1f - newW) / 2f, 0f, newW, 1f);
-        }
-        else{
-            float newH = ((float)setWidth / setHeight) / ((float)deviceW / deviceH);
-            Camera.main.rect = new Rect(0f, (1f - newH) / 2f, 1f, newH);
-        }
-        */
     }
 
     // Update is called once per frame
