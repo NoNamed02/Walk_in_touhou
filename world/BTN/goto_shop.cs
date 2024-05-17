8d3142c384480cba231fb716cedd915b6ac370a0
[NoNamed02] <[knite7989@gmail.com]>
2024-05-18 01:15:28 +0900

    카메라 관리자 이름 변경

--- world/BTN/goto_shop.cs
+++ world/BTN/goto_shop.cs
@@ -4,11 +4,11 @@ using UnityEngine.SceneManagement;
 public class goto_shop : MonoBehaviour
 {
     public GPSManager player;
-    public CameraSwitch cameraswitch;
+    public CameraSwitch C;
     // 버튼을 눌렀을 때 호출되는 함수
     public void GoTo()
     {
         player.ismove = false;
-        cameraswitch.isCamera1Active = 2;
+        C.isCameraActive = 2;
     }
 }
