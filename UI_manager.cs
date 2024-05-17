eea9cebc7861bdd0ec909e6fcc9c9511ce8af9e0
[NoNamed02] <[knite7989@gmail.com]>
2024-05-18 01:11:46 +0900

    UI관리자 추가

---
+++ UI_manager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_manager : MonoBehaviour
+{
+    public GameObject World_UI;
+    public GameObject Shop_UI;
+    public GameObject Fight_UI;
+    public GPSManager player;
+    public CameraSwitch C;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.ismove == true && C.isCameraActive == 1){
+            World_UI.SetActive(true);
+            Shop_UI.SetActive(false); 
+            Fight_UI.SetActive(false);
+        }
+        else if(player.ismove == false && C.isCameraActive == 2){
+            World_UI.SetActive(false);
+            Shop_UI.SetActive(true);
+            Fight_UI.SetActive(false);
+        }
+        else if(player.ismove == false && C.isCameraActive == 3){
+            World_UI.SetActive(false);
+            Shop_UI.SetActive(false);
+            Fight_UI.SetActive(true);
+        }
+    }
+}
+
