b0d3d6fea6d60c5908120fc4f2dc8bc48e018917
[NoNamed02] <[knite7989@gmail.com]>
2024-05-18 01:15:10 +0900

    전투 진입 매커니즘 변경

--- world/monster/monster.cs
+++ world/monster/monster.cs
@@ -5,21 +5,23 @@ using UnityEngine.Android;
 
 public class monster : MonoBehaviour
 {
-    public Gamemanager gamemanager;
-    public GameObject Move_scencePrefab; // 이동할 씬을 나타내는 프리팹
-    public string canvasTag = "Canvas"; // Canvas 태그 이름
+    private Collider2D monsterCollider; // Monster의 Collider2D 컴포넌트
 
-    private GameObject canvasObject; // Canvas GameObject
+    public GameObject move_scence;
+
+    public GPSManager player;
+    public CameraSwitch cameraswitch;
 
     void Start()
     {
-        // Canvas를 찾아서 canvasObject 변수에 할당합니다.
-        canvasObject = GameObject.FindGameObjectWithTag(canvasTag);
-        
-        // Canvas를 찾지 못한 경우 오류 메시지를 표시합니다.
-        if (canvasObject == null)
+
+        // Collider2D 컴포넌트를 찾아서 monsterCollider 변수에 할당합니다.
+        monsterCollider = GetComponent<Collider2D>();
+
+        // Collider2D를 찾지 못한 경우 오류 메시지를 표시합니다.
+        if (monsterCollider == null)
         {
-            Debug.LogError("Canvas를 찾을 수 없습니다. Canvas GameObject에 " + canvasTag + " 태그를 지정하세요.");
+            Debug.LogError("Collider2D를 찾을 수 없습니다. Monster GameObject에 Collider2D 컴포넌트를 추가하세요.");
         }
     }
 
@@ -30,12 +32,14 @@ public class monster : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && canvasObject != null) // 충돌한 객체가 Player 태그를 가지고 있고 Canvas가 할당된 경우
+        if (other.CompareTag("Player")) // 충돌한 객체가 Player 태그
         {
-            GameObject moveScene = Instantiate(Move_scencePrefab, canvasObject.transform);
-            RectTransform rectTransform = moveScene.GetComponent<RectTransform>();
-            rectTransform.anchoredPosition = Vector2.zero; // Canvas의 중앙에 위치하도록 설정
-            Destroy(gameObject);
+            if (monsterCollider != null)
+            {
+                monsterCollider.enabled = false;
+                player.ismove = false;
+                move_scence.SetActive(true);
+            }
         }
     }
 }
