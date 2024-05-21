33279b9d0e91aa9993781ee7234e9d3a8130d478
[NoNamed02] <[knite7989@gmail.com]>
2024-05-21 12:37:58 +0900

    하쿠레이 신사 UI추가

--- UI_manager.cs
+++ UI_manager.cs
@@ -7,6 +7,7 @@ public class UI_manager : MonoBehaviour
     public GameObject World_UI;
     public GameObject Shop_UI;
     public GameObject Fight_UI;
+    public GameObject Hakurei_UI;
     public GPSManager player;
     public CameraSwitch C;
 
@@ -17,16 +18,25 @@ public class UI_manager : MonoBehaviour
             World_UI.SetActive(true);
             Shop_UI.SetActive(false); 
             Fight_UI.SetActive(false);
+            Hakurei_UI.SetActive(false);
         }
         else if(player.ismove == false && C.isCameraActive == 2){
             World_UI.SetActive(false);
             Shop_UI.SetActive(true);
             Fight_UI.SetActive(false);
+            Hakurei_UI.SetActive(false);
         }
         else if(player.ismove == false && C.isCameraActive == 3){
             World_UI.SetActive(false);
             Shop_UI.SetActive(false);
             Fight_UI.SetActive(true);
+            Hakurei_UI.SetActive(false);
+        }
+        else if(player.ismove == false && C.isCameraActive == 4){
+            World_UI.SetActive(false);
+            Shop_UI.SetActive(false);
+            Fight_UI.SetActive(false);
+            Hakurei_UI.SetActive(true);
         }
     }
 }
