6a2690215ba1f3996467548102d9c331127c9ac4
[NoNamed02] <[knite7989@gmail.com]>
2024-05-21 12:37:27 +0900

    하쿠레이 신사 카메라 추가

--- camera_swich.cs
+++ camera_swich.cs
@@ -5,6 +5,7 @@ public class CameraSwitch : MonoBehaviour
     public Camera camera1; // 월드
     public Camera camera2; // 상점
     public Camera camera3; // 전투
+    public Camera camera4; // 미연시 
 
     public int isCameraActive = 1;
 
@@ -13,6 +14,7 @@ public class CameraSwitch : MonoBehaviour
         camera1.enabled = true;
         camera2.enabled = false;
         camera3.enabled = false;
+        camera4.enabled = false;
     }
 
     void Update()
@@ -21,17 +23,25 @@ public class CameraSwitch : MonoBehaviour
             camera1.enabled = true;
             camera2.enabled = false;
             camera3.enabled = false;
-
+            camera4.enabled = false;
         }
         else if(isCameraActive == 2){
             camera1.enabled = false;
             camera2.enabled = true;
             camera3.enabled = false;
+            camera4.enabled = false;
         }
         else if(isCameraActive == 3){
             camera1.enabled = false;
             camera2.enabled = false;
             camera3.enabled = true;
+            camera4.enabled = false;
+        }
+        else if(isCameraActive == 4){
+            camera1.enabled = false;
+            camera2.enabled = false;
+            camera3.enabled = false;
+            camera4.enabled = true;
         }
     }
 }
