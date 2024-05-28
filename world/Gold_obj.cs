c5f8aeef2c823c3348dc735418d90417e416ab09
[NoNamed02] <[knite7989@gmail.com]>
2024-05-29 01:36:27 +0900

    플레이어와 충돌시 좌표 변경 매커니즘 추가

--- world/Gold_obj.cs
+++ world/Gold_obj.cs
@@ -9,23 +9,31 @@ public class Gold_obj : MonoBehaviour
     public Collider2D col;
     void Start()
     {
+        float X = Random.Range(-21, 22);
+        float Y = Random.Range(-13, 17);
+        gameObject.transform.position = new Vector3(X, Y, -1f);
         Sound = GetComponent<AudioSource>();
         col = GetComponent<Collider2D>();
     }
 
     void Update()
     {
-        
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player")) //
         {
-            col.enabled = false;
+            //col.enabled = false;
             Sound.Play();
             NPCManager.Instance.Gold += 1;
             //Destroy(gameObject);
+            random_move();
         }
         Text.SetActive(true);
+    }
+    void random_move(){
+        float X = Random.Range(-21, 22);
+        float Y = Random.Range(-13, 17);
+        gameObject.transform.position = new Vector3(X, Y, -1f);
     }
 }
