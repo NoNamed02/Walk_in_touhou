914d3d9a52eeade19415df98ced6a49ed998368e
[NoNamed02] <[knite7989@gmail.com]>
2024-05-28 15:36:50 +0900

    몬스터 스포너 기능 수정

--- Fight/monster_manager.cs
+++ Fight/monster_manager.cs
@@ -1,37 +1,35 @@
 using UnityEngine;
 using System.Collections;
- 
-public class GameMgr : MonoBehaviour {
+
+public class monster_spwaner : MonoBehaviour {
     public Transform[] points;
     public GameObject monsterPrefab;
- 
+
     public float createTime;
     public int maxMonster = 10;
+    public int monsterCount;
 
-    public int check;
- 
     void Start () {
         points = GameObject.Find("SpawnPoint").GetComponentsInChildren<Transform>();
- 
-        if(points.Length > 0)
-        {
-            StartCoroutine(this.CreateMonster());
-        }
+        StartCoroutine(CreateMonster());
     }
- 
+    void Update(){
+        monsterCount = (int)GameObject.FindGameObjectsWithTag("enemy").Length;
+    }
     IEnumerator CreateMonster()
     {
-            int monsterCount = (int)GameObject.FindGameObjectsWithTag("enemy").Length;
-            check = monsterCount;
             if(monsterCount < maxMonster)
             {
                 yield return new WaitForSeconds(createTime);
- 
                 int idx = Random.Range(1, points.Length);
                 Instantiate(monsterPrefab, points[idx].position, points[idx].rotation);
+                StartCoroutine(CreateMonster());
             }else
             {
-                yield return null;
+                Debug.Log("몬스터 안나옴");
+                //yield return null;
+                yield return new WaitForSeconds(createTime);
+                StartCoroutine(CreateMonster());
             }
     }
 }
