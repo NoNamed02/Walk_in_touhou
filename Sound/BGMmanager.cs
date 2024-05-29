65616c6e86e1abe4425137994c2ac2b80f2e9f4c
[NoNamed02] <[knite7989@gmail.com]>
2024-05-30 01:01:06 +0900

    싱글톤 제거, 재생 방식 변경

--- Sound/BGMmanager.cs
+++ Sound/BGMmanager.cs
@@ -5,43 +5,50 @@ using UnityEngine;
 
 public class BGMmanager : MonoBehaviour
 {
-    private static BGMmanager instance;
+    //private static BGMmanager instance;
+    public static BGMmanager Instance;
     public CameraSwitch C;
-
-    public static BGMmanager Instance
-    {
-        get
-        {
-            if (instance == null)
-            {
-                GameObject singletonObject = new GameObject();
-                instance = singletonObject.AddComponent<BGMmanager>();
-                singletonObject.name = typeof(BGMmanager).ToString() + " (Singleton)";
-                DontDestroyOnLoad(singletonObject);
-            }
-            return instance;
-        }
-    }
-    
     
     public AudioSource Sound;
     public AudioClip Main;
+    public AudioClip Hakurei;
     public string sound_name;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
 
+            // AudioSource가 null일 경우 추가
+            if (Sound == null)
+            {
+                Sound = gameObject.AddComponent<AudioSource>();
+            }
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
     public void Playsound(string n){
         sound_name = n;
         switch(sound_name){
-            case "Main":
+            case "Main":{
                 Sound.clip = Main;
-                break;
+                break;}
+            case "Hakurei":{
+                Sound.clip = Hakurei;
+                break;}
         }
         Sound.Play();
     }
-
+    /*
     void Update(){
         if(!Sound.isPlaying && C.isCameraActive == 1){
             Sound.Stop();
             Playsound("Main");
         }
     }
+    */
 }
